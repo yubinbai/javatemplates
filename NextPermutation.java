a108b114d27487329b4aa66e55130b9a88b3df5e
Yubin Bai <baiyubin@gmail.com>
2014-08-10 14:47:53 -0400

    Next perm

---
+++ NextPermutation.java
@@ -0,0 +1,70 @@
+// Next Permutation
+// Implement next permutation, which rearranges numbers into the
+// lexicographically next greater permutation of numbers.
+
+// If such arrangement is not possible, it must rearrange it as the lowest
+// possible order (ie, sorted in ascending order).
+
+// The replacement must be in-place, do not allocate extra memory.
+
+// Here are some examples. Inputs are in the left-hand column and
+// its corresponding outputs are in the right-hand column.
+// 1,2,3 => 1,3,2
+// 3,2,1 => 1,2,3
+// 1,1,5 => 1,5,1
+
+import java.util.*;
+
+public class NextPermutation {
+    // get the next permutation in place
+    public static void nextPermutation(ArrayList<Integer> num) {
+        int size = num.size();
+        if (size <= 1) return;
+
+        // 1.find the first acending order pair from the bottom
+        int low = size - 2;
+        int high = size - 1;
+        while (num.get(low) >= num.get(high)) {
+            if (low == high - 1) {
+                low -= 1;
+                high = size - 1;
+                if (low < 0) break;
+            } else {
+                high -= 1;
+            }
+        }
+
+        // reached end of permutation
+        if (low < 0) {
+            Collections.reverse(num);
+            return;
+        }
+
+        // 2.swap num[lo] with num[hi] if lo >= 0
+        Collections.swap(num, low, high);
+
+        // 3.reverse lo+1 .. end
+        low = low + 1;
+        high = size - 1;
+        while (low < high) {
+            Collections.swap(num, low, high);
+            low++;
+            high--;
+        }
+
+        return;
+    }
+
+    public static void main(String[] args) {
+        int[] seq = {1, 2, 2, 3};
+        ArrayList<Integer> t = new ArrayList<Integer>();
+        for (int i : seq) {
+            t.add(i);
+        }
+        for (int i = 0; i < 29; i++) {
+            System.out.println(t);
+            NextPermutation.nextPermutation(t);
+        }
+    }
+
+}
