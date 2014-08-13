addd2774c6e5988b3b87daf98eece634a8641aa0
Yubin Bai <ybai@thrillist.com>
2014-08-13 18:26:48 -0400

    dup

--- SlidingWindow/duplicateInArray/RemoveDuplicate.java
+++ SlidingWindow/duplicateInArray/RemoveDuplicate.java
@@ -1,24 +1,34 @@
-package algorithm.sliding.window.duplicateInArray;
-
-/**
- * Hello world!
- * 
- */
+import java.util.*;
 public class RemoveDuplicate {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
 
-	public static int process(Integer[] data) {
-		// remove adjacent duplicates in an array
-		// use the sliding windows data[0..i] is processed region
+    public static int process(Integer[] data) {
+        // remove adjacent duplicates in an array
+        // use the sliding windows data[0..i] is processed region
 
-		int i = 0, j = 0, size = data.length;
-		for (j = 0; j < size; j++)
-			if (data[i] != data[j]) {
-				i++;
-				data[i] = data[j];
-			}
-		return i + 1;
-	}
+        int i = 0, j = 0, size = data.length;
+        for (j = 0; j < size; j++)
+            if (data[i] != data[j]) {
+                i++;
+                data[i] = data[j];
+            }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int size = 100;
+        Integer[] data = new Integer[size];
+
+        Random rand = new Random();
+        for (int i = 0; i < size; i++)
+            data[i] = rand.nextInt(size / 2);
+
+        Arrays.sort(data);
+        int newSize = RemoveDuplicate.process(data);
+        // for (int i = 0; i < size; i++)
+        // System.out.println(data[i]);
+
+        for (int i = 0; i < newSize - 1; i++)
+            for (int j = i + 1; j < newSize; j++)
+                if (data[i] == data[j]) System.out.println("Errors");
+    }
 }
