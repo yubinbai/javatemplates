d086e104047fe2c3850e8a19f8f6ead8623ccde2
Yubin Bai <ybai@thrillist.com>
2015-06-11 18:11:45 -0400

    segment tree rmq

--- SegmentTreeMin.java
+++ SegmentTreeMin.java
@@ -1,36 +1,68 @@
+import java.util.*;
+
 class SegmentTreeMin {
+    int size;
+    int[] tree;
 
-    public void initialize(int node, int b, int e, int M[], int A[], int N) {
-        if (b == e)
-            M[node] = b;
-        else {
-            int mid = (b + e) / 2;
-            initialize(2 * node, b, mid, M, A, N);
-            initialize(2 * node + 1, mid + 1, e, M, A, N);
-            if (A[M[2 * node]] <= A[M[2 * node + 1]]) {
-                M[node] = M[2 * node];
-            } else {
-                M[node] = M[2 * node + 1];
-            }
-        }
+    public SegmentTreeMin(int size) {
+        this.size = 1;
+        while (this.size < size) this.size <<= 1;
+
+        tree = new int[this.size * 2 - 1];
+        Arrays.fill(tree, Integer.MAX_VALUE);
     }
-    public int query(int node, int b, int e, int M[], int A[], int i, int j) {
-        int p1, p2;
+    /**
+     * set value at i (zero-based) to be a
+     * @param i
+     * @param a
+     */
+    public boolean set(int i, int a) {
+        if (i >= size) return false;
+        i += size - 1;
+        // leaf
+        tree[i] = a;
+        // parents
+        while (i > 0) {
+            i = (i - 1) / 2;
+            tree[i] = Math.min(tree[2 * i + 1], tree[2 * i + 2]);
+        }
+        return true;
+    }
 
-        //if the current interval doesn't intersect the query interval return -1
-        if (i > e || j < b) return -1;
+    /**
+     * find min in [begin, end) , with limit inside the range [low, high)
+     * @param  begin
+     * @param  end
+     * @param  node
+     * @param  low   
+     * @param  high   
+     */
+    private int query(int begin, int end, int node, int low, int high) {
+
+        //if the current interval doesn't intersect the query interval return 
+        if (high <= begin || low >= end) return Integer.MAX_VALUE;
 
         //if the current interval is included in the query interval return M[node]
-        if (b >= i && e <= j) return M[node];
+        if (low >= begin && high <= end) return tree[node];
 
-        //compute the minimum position in the left and right part of the interval
-        p1 = query(2 * node, b, (b + e) / 2, M, A, i, j);
-        p2 = query(2 * node + 1, (b + e) / 2 + 1, e, M, A, i, j);
-
-        //return the position where the overall minimum is
-        if (p1 == -1) return M[node] = p2;
-        if (p2 == -1) return M[node] = p1;
-        if (A[p1] <= A[p2]) return M[node] = p1;
-        return M[node] = p2;
+        int p1 = query(begin, end, node * 2 + 1, low, (low + high) / 2);
+        int p2 = query(begin, end, node * 2 + 2, (low + high) / 2, high);
+        return Math.min(p1, p2);
+    }
+    public int findMin(int begin, int end) {
+        return query(begin, end, 0, 0, size);
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        SegmentTreeMin sgm = new SegmentTreeMin(n);
+        Random r = new Random();
+        for (int i = 0; i < n; ++i) {
+            int t = r.nextInt(n * n);
+            System.out.format("#%d->%d \t", i, t);
+            sgm.set(i, t);
+        }
+        int begin = 0, end = 2;
+        System.out.println();
+        System.out.format("begin: %d, end: %d, value: %d\n", begin, end, sgm.findMin(0, 2)); 
     }
 }
